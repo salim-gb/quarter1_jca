f2a6bdb998e263cb9a615d9539f78fc141aef4fe
salim-gb <salimalsado.gb@gmail.com>
2021-07-17 23:46:35 +0300

    home work lesson 9

---
+++ src/main/java/q1_jca_l9/MyArraySizeException.java
@@ -0,0 +1,22 @@
+package q1_jca_l9;
+
+public class MyArraySizeException extends RuntimeException{
+    public MyArraySizeException() {
+    }
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+    public MyArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyArraySizeException(Throwable cause) {
+        super(cause);
+    }
+
+    public MyArraySizeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
